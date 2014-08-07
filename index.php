2ef09d04fdbb550ca76f7352815044b792dd7ec2
ManuDavila <manudg_1@msn.com>
2014-08-07 14:55:03 +0200

    Upload files
    Upload files

---
+++ index.php
@@ -0,0 +1,85 @@
+﻿<?php
+
+/**
+* Author: ManuDavila
+* Github: https://github.com/ManuDavila/json-file-reader
+* Website: http://jquery-manual.blogspot.com
+**/
+
+require "json-file-reader.class.php";
+
+/* Create json file */
+$json = array
+    (
+	 "images" => array
+	   (
+	    ["images/php.png", "PHP is the best"],
+		["images/zend.png", "Zend Framework"],
+		["images/yii.png", "Yii Framework"],
+		["images/symfony.png", "Symfony Framework"],
+	   ),
+	);
+	
+$json = json_encode($json);
+
+$handler = fopen("images.json", "w");
+fwrite($handler, $json);
+fclose($handler);
+/* Create json file */
+
+/* Now reader the json file */
+$reader = new json_file_reader();
+$json = $reader->json("images.json");
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>json file reader</title>
+<style>
+body {
+margin: 0;
+padding: 0;
+}
+
+.container
+{
+margin: 20px;
+}
+</style>
+</head>
+<body>
+<div class="container">
+<h1>php json file reader</h1>
+<h5>This class allow open and read a json file and extract the content it in array format</h5>
+<h3>Use: </h3>
+<pre>
+$reader = new json_file_reader();
+$json = $reader->json("images.json");
+print_r($json);
+</pre>
+<h3>images.json</h3>
+<?php print_r($json) //show images.json content ?>
+
+<h3>My Gallery</h3>
+<?php
+/* Get the images */
+foreach ($json as $key => $val)
+{
+    if ($key == "images")
+	 {
+	  foreach ($json['images'] as $i => $v)
+	  {
+	  print "<img src='".$json[$key][$i][0]."' rel='".$json[$key][$i][1]."' title='".$json[$key][$i][1]."'>\n";
+	  }
+	 }
+}
+?>
+<br>
+<br>
+<a href="https://github.com/ManuDavila/json-file-reader" target="_blank"><strong>Github</strong></a> | <a href="http://www.phpclasses.org/browse/author/1255792.html" target="_blank"><strong>phpclasses.org</strong></a>
+<br>
+<br>
+</div>
+</body>
+</html>
---
+++ json-file-reader.class.php
@@ -0,0 +1,41 @@
+﻿<?php 
+
+/**
+* Author: ManuDavila
+* Github: https://github.com/ManuDavila/json-file-reader
+* Website: http://jquery-manual.blogspot.com
+**/
+
+class json_file_reader {
+ 
+    public function json($json)
+	{
+	$array = array();
+	$handler = fopen($json, "r");
+	$content = null;
+	while (!feof($handler))
+	 {
+	 $content .= fgets($handler);
+	 }
+	 fclose($handler);
+	
+	 $reader = $this->json_decode($content);
+	 foreach($reader as $val)
+	 {
+	  $array[] = $reader;
+	 }
+	 return $array[0];
+	}
+ 
+    private function json_decode($json)
+    {
+        return json_decode($this->removeTrailingCommas(utf8_encode($json)), true);
+    }
+   
+    private function removeTrailingCommas($json)
+    {
+        $json=preg_replace('/,\s*([\]}])/m', '$1', $json);
+        return $json;
+    }
+
+}
